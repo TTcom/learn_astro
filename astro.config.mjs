e3f011adfb7df12be04eae72aa26daf2c411cbf2
jackAlmost <patiencetong10@gmail.com>
2023-05-25 11:18:15 +0800

    增加文件导入方式

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import UnoCSS from 'unocss/astro'
+
+// const modules = import.meta.glob('./preload.js')
+// console.log('modules',modules)
+// console.log('meta.glob.102', modules['./preload.js']())
+// modules['./preload.js']().then(data=>{
+//     console.log('ddddddddd',data.obj)
+// })
 // https://astro.build/config
 export default defineConfig({
     integrations: [
